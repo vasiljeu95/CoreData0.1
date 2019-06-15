bb392a9ebd5303ad4ea18a7e314c74f129209a3a
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-06-15 15:03:30 +0300

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Stepan Vasiljeu on 6/15/19.
+//  Copyright © 2019 Stepan Vasiljeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
